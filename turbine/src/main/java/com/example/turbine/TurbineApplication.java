12ced624e87068e1c0e2bd70670bd3aa0e62b86e
zhaoyue <zhaoyue@glzc.com>
2018-05-21 16:46:48 +0800

    add rabbit

--- turbine/src/main/java/com/example/turbine/TurbineApplication.java
+++ turbine/src/main/java/com/example/turbine/TurbineApplication.java
@@ -2,10 +2,10 @@ package com.example.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbine
+@EnableTurbineStream
 public class TurbineApplication {
 
   public static void main(String[] args) {
